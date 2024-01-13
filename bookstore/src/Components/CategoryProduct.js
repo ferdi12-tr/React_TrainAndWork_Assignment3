51f9a1458e440a2640190f29193ce976f4ca6910
fkoca <ferdikoca12@gmail.com>
2024-01-13 17:13:04 +0300

    display product by cateyoy

--- bookstore/src/Components/CategoryProduct.js
+++ bookstore/src/Components/CategoryProduct.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 export default class CategoryProduct extends Component {
     render() {
+        const { product } = this.props
         return (
             <div className="single-slide">
                 <div className="product-card card-style-list">
                     <div className="card-image">
-                        <img src="./image/products/product-1.jpg" alt="" />
+                        <img src={"image/products/" + product?.image} alt="" />
                     </div>
                     <div className="product-card--body">
                         <div className="product-header">
-                            <a href="#" className="author">
-                                Fpple
+                            <a href="" className="author">
+                                {product?.authorName}
                             </a>
-                            <h3><a href="product-details.html">5 Ways To Get
-                                Through To Your BOOK</a></h3>
+                            <h3>
+                                <Link to={"productDetail/" + product?.slug}>{product?.name}</Link>
+                            </h3>
                         </div>
                         <div className="price-block">
-                            <span className="price">£51.20</span>
-                            <del className="price-old">£51.20</del>
+                            <span className="price">£{product?.price}</span>
+                            <del className="price-old">£{product?.price + 10}</del>
                             <span className="price-discount">20%</span>
                         </div>
                     </div>
